0b0be3ce504efa9e7afe263cacbbb4f64287975c
Garrett Birkel <gwbirkel@lbl.gov>
2026-01-22 15:06:41 -0800

    Sort input paths into files vs. folders. If we're given more then one folder, exit. If we're given exactly one folder, hand it to the iterator for the given spec. If we're given no folders but some number of files, pass them directly to the ingestor (and assume it will handle them).

--- src/scicat_beamline/ingest.py
+++ src/scicat_beamline/ingest.py
@@ -4,8 +4,8 @@ import os
 import tempfile
 from pathlib import Path
 from typing import Any, Dict, List
-
 import typer
+
 from pyscicat.client import from_credentials
 
 from scicat_beamline.ingesters import (als_733_saxs_ingest,
@@ -62,16 +62,12 @@ def ingest(
 ):
     results: Dict[str, Any] = {}
 
-    # Normalize to single path (list support for specific ingesters coming later)
-    if isinstance(dataset_path, list):
-        dataset_path = dataset_path[0]
-
     if logger is None:
         logger = logging.getLogger("scicat_operation")
         logger.setLevel("INFO")
 
     if not ingester_spec:
-        scicat_url = os.getenv("SCICAT_INGEST_SPEC", "")
+        ingester_spec = os.getenv("SCICAT_INGEST_SPEC", "")
         if not ingester_spec:
             logger.exception("Cannot resolve ingester spec.")
             return results
@@ -100,65 +96,126 @@ def ingest(
             logger.info("Using SciCat username as owner username.")
             owner_username = scicat_username
 
-    if "SCICAT_INGEST_INTERNAL_BASE_FOLDER" in os.environ:
-        dataset_full_path = Path(os.getenv("SCICAT_INGEST_INTERNAL_BASE_FOLDER", "."), dataset_path).resolve()
-        logger.info(f"Using internal base folder; resolved dataset path: {dataset_full_path}")
-    elif "SCICAT_INGEST_BASE_FOLDER" in os.environ:
-        dataset_full_path = Path(os.getenv("SCICAT_INGEST_BASE_FOLDER", "."), dataset_path).resolve()
-        logger.info(f"Using base folder; resolved dataset path: {dataset_full_path}")
-    else:
-        dataset_full_path = Path(dataset_path).resolve()
-        logger.info(f"No base folder set; resolved dataset path: {dataset_full_path}")
-
-    # At the same time we're streaming logs to the console,
-    # we'll write them to a file in the dataset folder.
-
-    logger.info("Setting up ingester logfile.")
-
-    logfile = Path(dataset_full_path, "scicat_ingest_log.log")
-    formatter = logging.Formatter(
-        fmt="%(asctime)s [%(levelname)s] %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p"
-    )
-    fileHandler = logging.FileHandler(
-        logfile, mode="a", encoding=None, delay=False, errors=None
-    )
-    fileHandler.setFormatter(formatter)
-    logger.addHandler(fileHandler)
-
     logger.info(f"Using ingester spec {ingester_spec}")
 
-    try:
-        ingestion_function = None
+    ingestion_function = None
+
+    if ingester_spec == "bltest":
+        ingestion_function = als_test_ingest
+    elif ingester_spec == "als_11012_igor":
+        ingestion_function = als_733_saxs_ingest
+    elif ingester_spec == "als_832_dx_4":
+        ingestion_function = als_832_dx_4_ingest
+    elif ingester_spec == "als_11012_scattering":
+        ingestion_function = als_11012_scattering_ingest
+    elif ingester_spec == "als_11012_nexafs":
+        ingestion_function = nexafs_ingest
+    elif ingester_spec == "nsls2_rsoxs_sst1":
+        ingestion_function = nsls2_rsoxs_sst1_ingest
+    elif ingester_spec == "nsls2_nexafs_sst1":
+        ingestion_function = nsls2_nexafs_sst1_ingest
+    elif ingester_spec == "als733_saxs":
+        ingestion_function = als_733_saxs_ingest
+    elif ingester_spec == "nsls2_trexs_smi":
+        ingestion_function = nsls2_TREXS_smi_ingest
+    elif ingester_spec == "polyfts_dscft":
+        ingestion_function = polyfts_dscft_ingest
+    else:
+        logger.exception(f"Cannot resolve ingester spec {ingester_spec}")
+        return results
+
+    prepend_path = None
+    if "SCICAT_INGEST_INTERNAL_BASE_FOLDER" in os.environ:
+        prepend_path = os.getenv("SCICAT_INGEST_INTERNAL_BASE_FOLDER", ".")
+        logger.info(f"Using internal base folder: {prepend_path}")
+    elif "SCICAT_INGEST_BASE_FOLDER" in os.environ:
+        prepend_path = os.getenv("SCICAT_INGEST_BASE_FOLDER", ".")
+        logger.info(f"Using base folder: {prepend_path}")
+    else:
+        logger.info(f"No base folder set.")
+
+    if not isinstance(dataset_path, list):
+        dataset_path = [dataset_path]
+
+    folders = []
+    files = []
+
+    for one_path in dataset_path:
+        if prepend_path:
+            full_path = Path(prepend_path, one_path).resolve()
+        else:
+            full_path = one_path.resolve()
+        logger.info(f"Resolved dataset file path: {full_path}")
+
+        if not full_path.exists():
+            logger.error(f"Given a path does not exist: {full_path}")
+            continue
+
+        if full_path.is_symlink():
+            logger.warning(f"Symlink detected. (Should not happen!) Skipping: {full_path}")
+        elif full_path.is_file():
+            files.append(full_path)
+        elif full_path.is_dir():
+            folders.append(full_path)
+
+    if len(folders) + len(files) == 0:
+        logger.error("No valid files or folders to ingest.")
+        return results
+
+    if len(folders) > 1:
+        logger.error("Given more than one folder to ingest. Only one folder is supported.")
+        return results
+
+    elif len(folders) == 0: # Only files given
+        # We're going to assume that every file handed directly to the ingester
+        # is valid for the ingester spec.
+        ingest_files_iter = files
+
+    else: # One folder given
+        ingestion_search_path = folders[0]
+
+        # If we've been given one folder, we attempt to write a logfile to it.
+        # At the same time we're streaming logs to the console, we'll write them here.
+
+        logger.info("Setting up ingester logfile.")
+
+        logfile = Path(ingestion_search_path, "scicat_ingest_log.log")
+        formatter = logging.Formatter(
+            fmt="%(asctime)s [%(levelname)s] %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p"
+        )
+        fileHandler = logging.FileHandler(
+            logfile, mode="a", encoding=None, delay=False, errors=None
+        )
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+
+        # Since we haven't been handed a list of files, we need to walk the folder
+        # and make our own.  The criteria vary based on the ingester spec.
+
         ingest_files_iter = []
 
         if ingester_spec == "bltest":
-            temp_iter = standard_iterator(f"{dataset_full_path}/*.txt")
+            temp_iter = standard_iterator(f"{ingestion_search_path}/*.txt")
             for file_str in temp_iter:
                 ingest_files_iter.append(file_str)
-            ingestion_function = als_test_ingest
 
         elif ingester_spec == "als_11012_igor":
-            ingest_files_iter = standard_iterator(f"{dataset_full_path}/CCD/*/dat/")
-            ingestion_function = als_733_saxs_ingest
+            ingest_files_iter = standard_iterator(f"{ingestion_search_path}/CCD/*/dat/")
 
         elif ingester_spec == "als_832_dx_4":
-            ingest_files_iter = standard_iterator(f"{dataset_full_path}/*/")
-            ingestion_function = als_832_dx_4_ingest
+            ingest_files_iter = standard_iterator(f"{ingestion_search_path}/*/")
 
         elif ingester_spec == "als_11012_scattering":
-            ingest_files_iter = standard_iterator(f"{dataset_full_path}/CCD/*/")
-            ingestion_function = als_11012_scattering_ingest
+            ingest_files_iter = standard_iterator(f"{ingestion_search_path}/CCD/*/")
 
         elif ingester_spec == "als_11012_nexafs":
-            ingest_files_iter = standard_iterator(f"{dataset_full_path}/Nexafs/*")
-            ingestion_function = nexafs_ingest
+            ingest_files_iter = standard_iterator(f"{ingestion_search_path}/Nexafs/*")
 
         elif ingester_spec == "nsls2_rsoxs_sst1":
-            ingest_files_iter = standard_iterator(f"{dataset_full_path}/*/")
-            ingestion_function = nsls2_rsoxs_sst1_ingest
+            ingest_files_iter = standard_iterator(f"{ingestion_search_path}/*/")
 
         elif ingester_spec == "nsls2_nexafs_sst1":
-            temp_iter = standard_iterator(f"{dataset_full_path}/*")
+            temp_iter = standard_iterator(f"{ingestion_search_path}/*")
             for file_str in temp_iter:
                 if (
                     file_str.endswith(".log")
@@ -167,34 +224,31 @@ def ingest(
                 ):
                     continue
                 ingest_files_iter.append(file_str)
-            ingestion_function = nsls2_nexafs_sst1_ingest
 
         elif ingester_spec == "als733_saxs":
-            temp_iter = standard_iterator(f"{dataset_full_path}/*.txt")
+            temp_iter = standard_iterator(f"{ingestion_search_path}/*.txt")
             for file_str in temp_iter:
                 ingest_files_iter.append(file_str)
-            ingestion_function = als_733_saxs_ingest
 
         elif ingester_spec == "nsls2_trexs_smi":
-            ingest_files_iter = standard_iterator("{dataset_full_path}/*/")
-            ingestion_function = nsls2_TREXS_smi_ingest
+            ingest_files_iter = standard_iterator("{ingestion_search_path}/*/")
 
         elif ingester_spec == "polyfts_dscft":
-            ingest_files_iter = standard_iterator(f"{dataset_full_path}/*/")
-            ingestion_function = polyfts_dscft_ingest
+            ingest_files_iter = standard_iterator(f"{ingestion_search_path}/*/")
 
         else:
             logger.exception(f"Cannot resolve ingester spec {ingester_spec}")
             return results
 
+    try:
         pyscicat_client = from_credentials(scicat_url, scicat_username, scicat_password)
 
         with tempfile.TemporaryDirectory() as temp_dir:
+            temp_path = Path(temp_dir)
             issues: List[Issue] = []
             dataset_id = None
             for ingest_file_str in ingest_files_iter:
                 ingest_file_path = Path(ingest_file_str)
-                temp_path = Path(temp_dir)
                 if ingest_file_path.exists():
                     logger.info(f"Ingesting {ingest_file_path}")
                     dataset_id = ingestion_function(
